3e72f289bd0d2d715298db4e8b00530869aa0c2f
cougar91 <alpha.z91@mail.ru>
2019-02-26 17:13:15 +0600

    Change_2

---
+++ BinarPos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace BinarPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int letter = 97, count = 0;
+            StreamReader sr = new StreamReader("input.txt");
+            string str = sr.ReadLine();
+            
+            using (StreamWriter sw = File.AppendText("output.txt"))
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if(str[i].ToString() == 1.ToString())
+                    {
+                        sw.Write(((char)(letter + count)).ToString());
+                        count = 0;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
