6cb31556fa2532f76c775fe8828bc0e5ebf38f59
cougar91 <alpha.z91@mail.ru>
2019-02-26 17:46:05 +0600

    Change_3

---
+++ Summa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Summa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            StreamWriter sw = new StreamWriter("output.txt");
+            int N = int.Parse(sr.ReadLine());
+            int count = 0;
+
+            if (N <= Math.Pow(10, 4))
+            {
+                if (N > 0)
+                {
+                    for (int i = 1; i <= N; i++)
+                    {
+                        count += i;
+                    }   
+                }
+                else
+                {
+                    for (int i = N; i <= 1; i++)
+                    {
+                        count += i;
+                    }
+                }
+                sw.Write(count);
+                sw.Close();
+            }
+        }
+    }
+}
